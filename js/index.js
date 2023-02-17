d363d02ddc6a9de5bb2bd551742c4a2ce3aecf3d
Jose Storani <jdstorani91@gmail.com>
2023-02-17 14:44:09 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-const opcionOtro = document.getElementById("opcionOtro");
-const contentOtro = document.getElementById("contentOtro");
+let opcionOtro = document.getElementById("opcionOtro")
+let contentOtro = document.getElementById("contentOtro");
 
 opcionOtro.addEventListener("click",() => {
     contentOtro.removeAttribute("hidden")
 })
+
+
+console.log("HOLA JS ANDA NO TE PREOCUPES")
