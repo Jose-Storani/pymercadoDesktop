6c9bf455614d30a8d59791f74564e093958e99cc
Jose Storani <jdstorani91@gmail.com>
2023-02-17 14:22:31 -0300

    revisiones finales

---
+++ index.js
@@ -0,0 +1,6 @@
+const opcionOtro = document.getElementById("opcionOtro");
+const contentOtro = document.getElementById("contentOtro");
+
+opcionOtro.addEventListener("click",() => {
+    contentOtro.removeAttribute("hidden")
+})
