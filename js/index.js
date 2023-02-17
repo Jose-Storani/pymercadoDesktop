abea2d47fadfb6607124d38e6c0e997aeec1a6af
Jose Storani <jdstorani91@gmail.com>
2023-02-17 15:12:56 -0300

    corregidas rutas links

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let opcionOtro = document.getElementById("opcionOtro")
-let contentOtro = document.getElementById("contentOtro");
-
-opcionOtro.addEventListener("click",() => {
-    contentOtro.style.display = "inline"
-})
-
